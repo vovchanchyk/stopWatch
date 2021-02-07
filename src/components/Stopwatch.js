65b8e918abfbae96c1772c59f6735e94f7c5b00a
Vovchanchyk <oberigfast@gmail.com>
2021-02-07 11:21:37 +0200

    '07.02'

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,86 +3,56 @@ import React, { useEffect, useState } from 'react';
 
 export default function Stopwatch(props) {
 
-    const [currentValue, setCurrentValue] = useState(135);
-  
-    let [mode, setMode] = useState('start');
-  
+const [time,setTime]=useState({h:0,m:0,s:0})
+const [mode,setMode]=useState()
 
-    const stop = () => {
-        setMode(mode = 'stop')
+let updateH = time.h, updateM =time.m, updateS = time.s;
+
+const run=()=>{
+    if(updateM === 60){
+        updateM = 0
+        updateH++
     }
-
-    const start = () => {
-        setMode('start')
-        setCurrentValue(currentValue + 1)
+    if(updateS === 60){
+        updateS = 0;
+        updateM++
     }
+    updateS++
+    return setTime({h:updateH,m:updateM,s:updateS})
+}
 
-    const pause = () => {
-        setMode(mode = 'pause')
-    }
+let intervalId
 
-    const reset = () => {
-        if (mode == 'pause') {
-            setMode('clean')
-        } else {
-            setMode('reset')
-        }
-    }
+const start=()=>{
+  setMode(setInterval(run,1000))
+}
+const stop=()=>{
+   
+  setMode(clearInterval(mode))
+}
 
-    let timer = () => {
-        let sec = setTimeout(() => {
-            if (mode === 'start') setCurrentValue(currentValue + 1);
-            if (mode === 'reset') setMode('start');
-        }, 1000);
-        return () => {
-            clearTimeout(sec);
-        }
-    }
- 
-    useEffect(() => {
-        if (currentValue >= 60) setCurrentValue(0)
-        switch (mode) {
-            case 'start': timer();
-                break;
-            case 'reset': setCurrentValue(0);
-                timer()
-                break;
-            case 'stop': setCurrentValue(0);
-                break;
-            default:
-                break;
-        }
-
-    }, [currentValue, mode])
-
-
-    let hours = Math.floor(currentValue/60/60).toString().padStart(2,'0');
-    let minutes = (Math.floor(currentValue/60) - (hours*60)).toString().padStart(2,'0');
-    let seconds = (currentValue%60).toString().padStart(2,'0')
-  
-console.log(minutes)
 
     return (
 
         <div className="stopwatch">
             <div className="stopwatch__display">
                 <div className="stopwatch__tabs">
-                    <span className="stopwatch__numbers">{hours}</span>
+                    <span className="stopwatch__numbers">{Math.floor(time.h/60/60).toString().padStart(2,'0')}</span>
                     <span className="stopwatch__colon">:</span>
                 </div>
                 <div className="stopwatch__tabs">
-                    <span className="stopwatch__numbers">{minutes}</span>
+                    <span className="stopwatch__numbers">{(Math.floor(time.m/60) - (time.h*60)).toString().padStart(2,'0')}</span>
                     <span className="stopwatch__colon">:</span>
                 </div>
                 <div className="stopwatch__tabs">
-                    <span className="stopwatch__numbers"></span>
+                    <span className="stopwatch__numbers">{(time.s%60).toString().padStart(2,'0')}</span>
                 </div>
 
             </div>
             <div className="stopwatch__btns">
                 <button className="stopwatch__btn" onClick={stop}>STOP</button>
                 <button className="stopwatch__btn" onClick={start}>START</button>
-                <button className="stopwatch__btn" onClick={reset}>RESET</button>
+                <button className="stopwatch__btn">RESET</button>
                 <button className="stopwatch__btn">WAIT</button>
             </div>
 
